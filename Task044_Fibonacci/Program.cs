7b17351d03c49737097200c6579431d749e756c1
MrQuart <36205339+AlexVashchenkov@users.noreply.github.com>
2022-08-21 23:32:57 +0300

    Add files via upload

---
+++ Task044_Fibonacci/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = 20;
+int firstElem = 1;
+int secondElem = 1;
+
+Console.WriteLine("Последовательность Фибоначчи");
+Console.WriteLine($"1. {firstElem}");
+Console.WriteLine($"2. {secondElem}");
+
+for (int i = 3; i <= number; i++)
+{
+    int nextElem = firstElem + secondElem;
+    Console.WriteLine($"{i}. {nextElem}");
+    firstElem = secondElem;
+    secondElem = nextElem;
+}
