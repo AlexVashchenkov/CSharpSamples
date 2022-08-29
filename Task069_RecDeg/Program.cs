260000d776f0b56ff91c8b1a9342a897dd4cd5e0
MrQuart <36205339+AlexVashchenkov@users.noreply.github.com>
2022-08-29 19:28:46 +0300

    Add files via upload

---
+++ Task069_RecDeg/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int Deg(int A, int B){
+    if (B == 1){
+        return A;
+    } else {
+        return A * Deg(A, B-1);
+    }
+}
+
+Console.WriteLine($"Число {A} в степени {B} = " + Deg(A, B));
